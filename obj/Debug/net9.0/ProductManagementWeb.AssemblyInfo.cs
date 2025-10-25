8dba5118aa775d948b6159fab5f16786397d15a7
John Titor <95267320+johntitor08@users.noreply.github.com>
2025-10-25 15:26:40 +0300

    .

--- obj/Debug/net9.0/ProductManagementWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProductManagementWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagementWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1aa6ccf8a8b262f95225eac4493c354d6c19ab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagementWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagementWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
